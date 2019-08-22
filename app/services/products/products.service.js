0654b238da2cee7ff69b71fc307debaac11d72ce
Darek Grund (PGS Software) <dgrund@pgs-soft.com>
2019-08-22 10:50:18 +0200

    Get Products from Firebase

--- app/services/products/products.service.js
+++ app/services/products/products.service.js
@@ -2,25 +2,39 @@
 
 angular.
   module('service.products', ['ngResource']).
-  factory('Product', ['$resource',
-    function($resource) {
+  factory('Product', ['$resource', 'API_URL',
+    function($resource, API_URL) {
       return {
-        getAll: function() {
-          return $resource('mock/products/all.json').query();
-        },
         getById: function(id) {
-          return $resource('mock/products/product-'+ id +'.json', {}, {
-            query: {
-              isArray: false
-            }
-          }).query();
+          return $resource(API_URL + '/products/:productId.json').get({productId: id});
         },
         getByCategoryId: function(id) {
-          return $resource('mock/products/category-'+ id +'.json').query();
+          return $resource(API_URL + '/products.json', {}, {
+            get: {
+              isArray: true,
+              transformResponse: function(data) {
+                var dataObject = angular.fromJson(data);
+                return dataObject.filter(function(item) {
+                  return item.category === id;
+                });
+              }
+            }
+          }).get();
         },
         getPromoted: function() {
-          return $resource('mock/products/promoted.json').query();
+          return $resource(API_URL + '/products.json', {}, {
+            get: {
+              isArray: true,
+              transformResponse: function(data) {
+                var dataObject = angular.fromJson(data);
+                return dataObject.filter(function(item) {
+                  return item.promoted === true;
+                });
+              }
+            }
+          }).get();
         }
       };
     }
   ]);
+
